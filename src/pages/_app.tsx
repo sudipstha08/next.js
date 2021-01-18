8603ebb05a3bddca7d55e452b4a5472f0d5fdbd6
Sudip Shrestha <sudipstha08@gmail.com>
2021-01-18 21:41:00 +0545

    add react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { AppProps } from 'next/app';
 import 'antd/dist/antd.css';
 import '../styles/main.scss';
 
-const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const queryClient = new QueryClient();
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
