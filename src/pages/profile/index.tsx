17a70cfb160a1ef395974850cfb1263c301e1072
Sudip Shrestha <sudipstha08@gmail.com>
2021-04-09 13:29:37 +0545

    use lodash

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,14 +1,23 @@
+/* eslint-disable no-console */
 import React from "react";
 import dynamic from "next/dynamic";
+import _ from "lodash";
 
 const ApexCharts = dynamic(
   () => import("../../components/molecules/ApexCharts/index"),
+  {
+    ssr: false,
+  },
 );
 
 const Profile = () => {
+  const greeting = _.join(["Hello", "World", "From", "React"], " ");
+  const chunkTest = _.chunk(["a", "b", "c", "d"], 2);
+  console.log("test", chunkTest);
   return (
     <div>
       <ApexCharts />
+      <h1>{greeting}</h1>
     </div>
   );
 };
