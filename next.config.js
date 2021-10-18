a455084be7a9fadae779eec6e1354b306f09a349
Sudip Shrestha <sudipstha08@gmail.com>
2021-10-18 18:22:02 +0545

    implement pwa

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
 const withPWA = require("next-pwa");
+const withImages = require("next-images");
+const withCSS = require("@zeit/next-css");
+const withSass = require("@zeit/next-sass");
+const withPlugins = require("next-compose-plugins");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const nextSourceMaps = require("@zeit/next-source-maps");
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = withPWA({
-  async rewrites() {
-    return [
-      {
-        source: "/sitemap.txt",
-        destination: "http://localhost:9001/sitemap",
-      },
-    ];
+const nextConfig = {
+  distDir: "../../build",
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
   },
   pwa: {
     dest: "public",
@@ -15,4 +19,30 @@ module.exports = withPWA({
     skipWaiting: true,
     disable: process.env.NODE_ENV === "development",
   },
-});
+  webpack: (config, { isServer }) => {
+    if (process.env.NODE_ENV === "production") {
+      config.devtool = "source-map";
+      config.optimization.minimize = true;
+      config.optimization.minimizer = [];
+      config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
+      config.optimization.minimizer.push(new TerserPlugin({ parallel: true }));
+    }
+    if (!isServer) {
+      config.resolve.alias["@sentry/node"] = "@sentry/browser";
+    }
+    return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/sitemap.xml",
+        destination: "http://localhost:9001/sitemap",
+      },
+    ];
+  },
+};
+
+module.exports = withPlugins(
+  [withCSS, withImages, withSass, nextSourceMaps, withPWA],
+  nextConfig,
+);
