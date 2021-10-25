8b558d9e65e42027499009c7b0b516a8e157f06d
Sudip Shrestha <sudipstha08@gmail.com>
2021-10-25 13:01:32 +0545

    image crop while upload image

--- src/components/molecules/ReactImageCrop/index.tsx
+++ src/components/molecules/ReactImageCrop/index.tsx
@@ -1,31 +1,66 @@
+/* eslint-disable no-console */
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable react/no-unescaped-entities */
-import React, { useState, useCallback, useRef, useEffect } from "react";
+import React, { useState, useCallback, useRef, useEffect, FC } from "react";
 import ReactCrop from "react-image-crop";
+import styled from "styled-components";
 import "react-image-crop/dist/ReactCrop.css";
+import { DeleteOutlined, UploadOutlined } from "@ant-design/icons";
+import { Spin, Upload, Modal } from "antd";
 
-function generateDownload(canvas, crop) {
-  if (!crop || !canvas) {
-    return;
-  }
-
-  canvas.toBlob(
-    (blob) => {
-      const previewUrl = window.URL.createObjectURL(blob);
-
-      const anchor = document.createElement("a");
-      anchor.download = "cropPreview.png";
-      anchor.href = URL.createObjectURL(blob);
-      anchor.click();
-
-      window.URL.revokeObjectURL(previewUrl);
-    },
-    "image/png",
-    1,
-  );
+interface IProps {
+  width?: any;
+  height?: any;
 }
 
-const ReactImageCrop = () => {
+const Wrapper = styled.div`
+  .upload-btn {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    bottom: 20px;
+    right: 60px;
+    width: 33px;
+    height: 33px;
+    border-radius: 50%;
+    background-color: #65b798;
+    border: 1px solid grey;
+    cursor: pointer;
+    & svg {
+      fill: white;
+      height: 15px;
+      width: 15px;
+    }
+  }
+  .delete-btn {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    bottom: 20px;
+    right: 15px;
+    width: 33px;
+    height: 33px;
+    border-radius: 50%;
+    border: 1px solid grey;
+    background-color: white;
+    cursor: pointer;
+    & svg {
+      fill: #cf1322;
+    }
+  }
+  & .ant-spin-nested-loading,
+  .ant-spin-container {
+    height: 100%;
+  }
+`;
+
+const ReactImageCrop: FC<IProps> = ({ width = "500px", height }) => {
   const [upImg, setUpImg] = useState("");
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const [file, setFile] = useState<any>(null);
   const imgRef = useRef(null);
   const previewCanvasRef = useRef(null);
   const [crop, setCrop] = useState<any>({
@@ -36,17 +71,46 @@ const ReactImageCrop = () => {
   });
   const [completedCrop, setCompletedCrop] = useState<any>(null);
 
-  const onSelectFile = (e) => {
-    if (e.target.files && e.target.files.length > 0) {
+  const onSelectFile = async (e) => {
+    if (e.file.status === "done") {
       const reader: any = new FileReader();
-      reader.addEventListener("load", () => setUpImg(reader?.result));
-      reader.readAsDataURL(e.target.files[0]);
+      await reader.addEventListener("load", () => setUpImg(reader.result));
+      await reader.readAsDataURL(e?.file?.originFileObj);
+      setIsModalVisible(true);
     }
   };
 
   const onLoad = useCallback((img) => {
     imgRef.current = img;
   }, []);
+
+  const handleOk = async (canvas, crop) => {
+    console.log("ok");
+    console.log("crop", crop);
+    console.log("canvas", canvas);
+    if (!crop || !canvas) {
+      return;
+    }
+    await canvas.toBlob(
+      (blob) => {
+        const previewUrl = window.URL.createObjectURL(blob);
+
+        const anchor = document.createElement("a");
+        anchor.download = "cropPreview.png";
+        anchor.href = URL.createObjectURL(blob);
+        anchor.click();
+
+        window.URL.revokeObjectURL(previewUrl);
+      },
+      "image/png",
+      1,
+    );
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
 
   useEffect(() => {
     if (!completedCrop || !previewCanvasRef.current || !imgRef.current) {
@@ -83,39 +147,76 @@ const ReactImageCrop = () => {
 
   return (
     <div className="App">
-      <div>
-        <input type="file" accept="image/*" onChange={onSelectFile} />
-      </div>
-      <ReactCrop
-        src={upImg}
-        onImageLoaded={onLoad}
-        crop={crop}
-        onChange={(c) => setCrop(c)}
-        onComplete={(c) => setCompletedCrop(c)}
-        disabled={false}
-        locked={true}
-        imageStyle={{ width: 700, objectFit: "contain" }}
-      />
+      <Wrapper
+        style={{
+          position: "relative",
+          width: width,
+          height: height,
+          background: "#E6E6E6",
+          border: "1px solid #D9D9D9",
+          borderRadius: "2px",
+          boxSizing: "border-box",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <Spin delay={500} className="image-spin-wrapper" spinning={false}>
+          <img
+            src={file ? file?.url : "/images/image-01.jpg"}
+            alt={file ? file?.alt : "Thumbnail image"}
+            style={{
+              height: "100%",
+              objectFit: "contain",
+              width: "100%",
+            }}
+          />
+
+          <Upload
+            showUploadList={false}
+            onChange={onSelectFile}
+            accept="image/*"
+          >
+            <div className="upload-btn">
+              <UploadOutlined />
+            </div>
+          </Upload>
+          <div className="delete-btn">
+            <DeleteOutlined />
+          </div>
+        </Spin>
+      </Wrapper>
       <div>
         <canvas
           ref={previewCanvasRef}
           style={{
             width: Math.round(completedCrop?.width ?? 0),
             height: Math.round(completedCrop?.height ?? 0),
+            display: "none",
           }}
         />
       </div>
-      <button
-        type="button"
-        disabled={!completedCrop?.width || !completedCrop?.height}
-        onClick={() =>
-          generateDownload(previewCanvasRef.current, completedCrop)
-        }
+
+      <Modal
+        title="Select the area to crop"
+        visible={isModalVisible}
+        onOk={() => handleOk(previewCanvasRef.current, completedCrop)}
+        onCancel={handleCancel}
       >
-        Download cropped image
-      </button>
+        <ReactCrop
+          src={upImg}
+          onImageLoaded={onLoad}
+          crop={crop}
+          onChange={(c) => setCrop(c)}
+          onComplete={(c) => setCompletedCrop(c)}
+          disabled={false}
+          locked={true}
+          imageStyle={{ width: 700, objectFit: "contain" }}
+        />
+      </Modal>
     </div>
   );
 };
 
 export { ReactImageCrop };
+// https://codepen.io/qertis/pen/RNPXee
